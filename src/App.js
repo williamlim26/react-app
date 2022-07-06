a582ba2071bd5e3a0531e82cd678068cf1f247be
William Lim <william.lim2@telus.com>
2022-07-05 21:45:47 -0700

    add upload artifacts

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          This is will
+          Learn React
         </a>
       </header>
     </div>
